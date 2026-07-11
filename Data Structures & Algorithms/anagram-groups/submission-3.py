64257e1879312dd8e5d1289f0a8e98a16fdac738
AleRus19 <a.russo19@outlook.it>
2026-07-11 16:46:28 +0200

    Add: anagram-groups - submission-3

---
+++ Data Structures & Algorithms/anagram-groups/submission-3.py
@@ -0,0 +1,40 @@
+class Solution:
+
+    def isAnagram(self, s: str, t: str) -> bool:
+        if (len(s) != len(t)):
+            return False
+    
+        d = {}
+
+        for x in s:
+            if not x in d:
+                d[x] = 1
+            else:
+                d[x] +=1
+
+        for y in t:
+            if not y in d:
+                return False
+            else:
+                d[y] -=1
+                if d[y] == 0:
+                    del d[y]
+
+        return sum(d.values()) == 0
+
+
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        res = []
+        for x in strs:
+            found = False
+            for g in res:
+                if self.isAnagram(x, g[0]):
+                    g.append(x)
+                    found = True
+                    break
+            if not found:
+                res.append([x])
+        return res
+                    
+
+        
