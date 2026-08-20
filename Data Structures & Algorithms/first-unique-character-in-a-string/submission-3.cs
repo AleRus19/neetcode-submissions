b3af0fd7eeaa7596a71400dd8b502e00233897db
AleRus19 <a.russo19@outlook.it>
2026-08-20 23:20:17 +0200

    Add: first-unique-character-in-a-string - submission-3

---
+++ Data Structures & Algorithms/first-unique-character-in-a-string/submission-3.cs
@@ -0,0 +1,22 @@
+public class Solution {
+    public int FirstUniqChar(string s) {
+        Dictionary<char, bool> dict = [];
+
+        var n = s.Length;
+        for (int i = 0; i < n; i++) {
+            if (dict.TryGetValue(s[i], out var _)) {
+                dict[s[i]] = false;
+            } else {
+                dict[s[i]] = true;
+            }
+        }
+        
+        for (int i = 0; i < n; i++) {
+            if (dict[s[i]] == true) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
